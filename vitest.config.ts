fe9ceffc6781109c48a5f8902f4d093d5095bc9a
David FYON <davidfyon@gmail.com>
2024-06-27 12:14:33 +0200

    add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,30 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import {fileURLToPath} from 'node:url'
+import {mergeConfig, defineConfig, configDefaults} from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
+    viteConfig,
+    defineConfig({
+        test: {
+            environment: 'jsdom',
+            exclude: [...configDefaults.exclude, 'e2e/**'],
+            root: fileURLToPath(new URL('./', import.meta.url)),
+            coverage: {
+                enabled: true,
+                provider: 'v8',
+                reportsDirectory: './tests/unit/coverage',
+                include: [
+                    '**/components/**',
+                    '**/views/**',
+                    '**/store/**',
+                ]
+            },
+            server: {
+                deps: {
+                    inline: ['vuetify']
+                }
+            },
+            setupFiles: ['./tests/unit/setup.ts']
+        }
+    })
 )
