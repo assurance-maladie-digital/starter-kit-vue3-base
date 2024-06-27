ed9a599030295639537959c48e7c4d31e7ffa24b
David FYON <davidfyon@gmail.com>
2024-06-27 14:12:20 +0200

    add test mock

--- src/views/tests/NotFoundView.spec.ts
+++ src/views/tests/NotFoundView.spec.ts
@@ -1,19 +1,33 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import NotFound from '../NotFoundView.vue'
 import { vuetify } from '../../../tests/unit/setup'
 
 describe('NotFoundView', () => {
+    const mockRouter = {
+        push: vi.fn()
+    };
+
+    const mockRoute = {
+        fullPath: '/test',
+        name: undefined,
+        path: '/test',
+    };
+
     it('renders properly', () => {
         const wrapper = mount(NotFound, {
             global: {
                 plugins: [
                     vuetify,
                 ],
-            }
+                mocks: {
+                    $router: mockRouter,
+                    $route: mockRoute
+                },
+            },
         })
         expect(wrapper.html()).toMatchSnapshot();
+        expect(mockRouter.push).toHaveBeenCalled();
 
         const h2Text = wrapper.find('h2').text();
         expect(h2Text).toContain('Page non trouvée');
