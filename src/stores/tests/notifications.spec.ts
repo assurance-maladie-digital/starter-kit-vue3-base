6d72b963da6ed25c1bbffe9c07caf0629c35a351
virginieVachet <vachetvirginie@gmail.com>
2024-07-08 15:59:36 +0200

    wip fix errors on build

---
+++ src/stores/tests/notifications.spec.ts
@@ -0,0 +1,18 @@
+import { createPinia } from 'pinia'
+import { useNotificationStore } from '../notifications'
+import { describe, it, expect, beforeEach } from 'vitest'
+
+describe('Notification Store', () => {
+    let store: ReturnType<typeof useNotificationStore>
+
+    beforeEach(() => {
+        const pinia = createPinia()
+        store = useNotificationStore(pinia)
+    })
+
+    it('should have an initial state of empty object', () => {
+        expect(store.$state).toEqual({
+            notificationPayload: {},
+        })
+    })
+})
