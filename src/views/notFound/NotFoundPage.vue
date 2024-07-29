266e8057beedca3a3e7f0a0840e870c91904fae5
adrien charrier <adrien.charrier@outlook.fr>
2024-07-29 16:41:52 +0200

    chore: Refactor component imports and update dependencies

--- src/views/notFound/NotFoundPage.vue
+++ src/views/notFound/NotFoundPage.vue
@@ -34,66 +34,17 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import { mapActions, mapGetters } from "vuex";
-import { required } from "@cnamts/synapse-bridge/rules/required";
-import { notAfterToday } from "@cnamts/synapse-bridge/rules/notAfterToday";
-import { notBeforeToday } from "@cnamts/synapse-bridge/rules/notBeforeToday";
-import dayjs from "dayjs";
 import {
 	PageContainer,
-	HeaderBar,
 	FooterBar,
-	DataList,
-	DataListGroup,
-	UserMenuBtn,
-	DialogBox,
-	FooterWrapper,
-	SubHeader,
-	FranceConnectBtn,
-	CopyBtn,
-	Logo,
-	HeaderLoading,
-	BackToTopBtn,
-	CookieBanner,
-	CookiesPage,
-	LogoBrandSection,
-	ChipList,
-	NotificationBar,
-	ErrorPage,
-	MaintenancePage,
 	NotFoundPage,
-	PasswordField,
-	DatePicker,
-	PeriodField
 } from "@cnamts/synapse-bridge";
 
 export default defineComponent({
 	components: {
-		PageContainer,
-		HeaderBar,
-		FooterBar,
-		UserMenuBtn,
-		DialogBox,
-		DataList,
-		DataListGroup,
-		FooterWrapper,
-		SubHeader,
-		FranceConnectBtn,
-		CopyBtn,
-		Logo,
-		HeaderLoading,
-		BackToTopBtn,
-		CookieBanner,
-		CookiesPage,
-		LogoBrandSection,
-		ChipList,
-		NotificationBar,
-		ErrorPage,
-		MaintenancePage,
 		NotFoundPage,
-		PasswordField,
-		DatePicker,
-		PeriodField
+		PageContainer,
+		FooterBar
 	},
 	data() {
 		return {
